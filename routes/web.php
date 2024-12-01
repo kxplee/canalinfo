6fafba3054ec23d92e89e61e38cd20c6aec943f3
Fede <fede@tlacorp.com>
2024-12-01 20:52:20 +0100

    Inicio3

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hola que tal desde el welcome";
 });
 
 Route::get('/test', function () {
